b3f591ae9f66ff0ef79e7df6ebb0f94f9ec829f2
Badr-Eddine Hazzouti <hazzoutibadreddine@gmail.com>
2023-08-09 01:11:43 +0100

    end

--- Makefile
+++ Makefile
@@ -6,18 +6,19 @@
 #    By: bhazzout <bhazzout@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/08 22:22:17 by bhazzout          #+#    #+#              #
-#    Updated: 2023/08/08 05:30:07 by bhazzout         ###   ########.fr        #
+#    Updated: 2023/08/09 01:09:05 by bhazzout         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 
 NAME	= cub3D
 
-CFLAGS	= -Wall -Wextra -Werror -Wuninitialized -fsanitize=address
+CFLAGS	= -Wall -Wextra -Werror -Wuninitialized #-fsanitize=address
 
 RM	= rm -rf
 
-SOURCES	= cub3d.c utils.c ft_split.c ft_split_utils.c gnl/get_next_line_utils.c gnl/get_next_line.c 
+SOURCES	= cub3d.c utils.c ft_split.c ft_split_utils.c gnl/get_next_line_utils.c gnl/get_next_line.c update.c move_handle.c player_render.c \
+		rays.c distance.c dist_hor.c dist_ver.c dist_ver_1.c pixel.c
 
 OBJECTS	= $(SOURCES:.c=.o)
 
--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: bhazzout <bhazzout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 22:12:42 by bhazzout          #+#    #+#             */
-/*   Updated: 2023/08/08 04:50:33 by bhazzout         ###   ########.fr       */
+/*   Updated: 2023/08/09 01:08:42 by bhazzout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,18 @@
 # define WIN_WIDTH 900
 # define WIN_HEIGHT 600
 # define CELL_SIZE 32
+# define MINI_HEIGHT 200
+# define MINI_WIDTH 300
+
+typedef struct s_move
+{
+	int	move_up;
+	int	move_down;
+	int	move_left;
+	int	move_right;
+	int	rotate_left;
+	int	rotate_right;
+}	t_move;
 
 typedef struct s_sqr
 {
@@ -127,29 +139,75 @@ typedef struct s_win
 	int			cell_size;
 	int			map_width;
 	int			map_height;
+	double		distance_towall;
+	int			color;
 	t_wall		wall;
 	t_image		img;
 	t_ray		ray;
-	double		distance_towall;
-	int			color;
+	t_move		mv;
 }	t_win;
 
-char	*get_next_line(int fd);
-
-char	*ft_strjoin(const char *s1, const char *s2);
-size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(char *s1);
-char	**map_getter(void);
-void	map_printer(char **map);
-char	**map2d_getter(int fd, int counter);
-int		calc_words(const char *s, char c);
-int		ft_wordlen(const char *s, int i, char c);
-char	*ft_charge(const char *s, int i, char c);
-void	*freeini(char **string, int len);
-char	**ft_split(const char *s, char sep);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-void	map_draw(t_win *win);
+char		*get_next_line(int fd);
+char		*ft_strjoin(const char *s1, const char *s2);
+size_t		ft_strlen(const char *str);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(char *s1);
+char		**map_getter(void);
+void		map_printer(char **map);
+char		**map2d_getter(int fd, int counter);
+int			calc_words(const char *s, char c);
+int			ft_wordlen(const char *s, int i, char c);
+char		*ft_charge(const char *s, int i, char c);
+void		*freeini(char **string, int len);
+char		**ft_split(const char *s, char sep);
+void		ft_bzero(void *s, size_t n);
+void		*ft_calloc(size_t count, size_t size);
+void		map_draw(t_win *win);
+void		x_y_update(t_win *win, double playerdx, double playerdy);
+void		move_update(t_win *win, double playerdx, double playerdy);
+int			p_update(t_win *win);
+int			key_release_handler(int keycode, t_win *win);
+int			key_handler(int keycode, t_win *win);
+int			mouse_handler(int x, int y, t_win *win);
+void		player_render(t_win *win);
+void		sqr_init(t_sqr *ray_sqr, t_win *win, t_mini *m_map);
+void		ray_draw(t_win *win, t_mini	*m_map);
+void		cells_draw(t_win *win, t_mini *m_map, int color);
+void		map_draw(t_win *win);
+void		wall_put(t_win *win, t_wallhit *wall_hit);
+void		floor_ceil(t_win *win, int flag);
+void		rays_init(t_win *win);
+t_wallhit	ray_const_init(t_win *win);
+void		hor_right_down(t_hit *hit_const, t_win *win,
+				double ray_angle, t_wallhit *hor);
+void		hor_dist_calcul(t_hit *hit_const, t_win *win,
+				double ray_angle, t_wallhit *hor);
+t_wallhit	horizontal_check(t_win *win, double ray_angle, t_wallhit *hor);
+t_wallhit	distance_getter(t_win *win, double ray_angle);
+void		hor_hit_const_init(t_hit *hit_const, t_wallhit *hor);
+int			hor_right_up(t_hit *hit_const, t_win *win,
+				double ray_angle, t_wallhit *hor);
+void		hor_left_up(t_hit *hit_const, t_win *win,
+				double ray_angle, t_wallhit *hor);
+int			hor_left_down(t_hit *hit_const, t_win *win,
+				double ray_angle, t_wallhit *hor);
+void		left_down(t_hit *hit_const, t_win *win, double ray_angle,
+				t_wallhit *ver);
+int			right_down(t_hit *hit_const, t_win *win, double ray_angle,
+				t_wallhit *ver);
+void		distance_calcul(t_hit *hit_const, t_win *win, double ray_angle,
+				t_wallhit *ver);
+void		ver_hit_const_init(t_hit *hit_const, t_wallhit *ver);
+t_wallhit	vertical_check(t_win *win, double ray_angle, t_wallhit *ver);
+void		right_up(t_hit *hit_const, t_win *win, double ray_angle,
+				t_wallhit *ver);
+int			left_up(t_hit *hit_const, t_win *win, double ray_angle,
+				t_wallhit *ver);
+double		ray_correct(double ray_angle);
+void		my_pixel_to_img(t_win *win);
+void		mini_pixel_to_img(t_win *win, int x, int y, int color);
+void		pixel_to_img(t_win *win, int x, int y);
+void		my_mlx_draw_line(t_win *win, t_wallhit *wallhit);
+int			windows_close(int keycode, void *param);
 
 #endif
